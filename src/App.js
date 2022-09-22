9a1b597a939c818c11c99540545f19360a300ea0
Gilles30 <lorison.gilles@proton.me>
2022-09-22 16:44:13 -0300

    🛹❤️ send email

---
+++ src/App.js
@@ -0,0 +1,78 @@
+import { useState } from 'react'
+import emailjs from '@emailjs/browser'
+import './App.css'
+
+function App() {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [message, setMessage] = useState('')
+
+  function sendEmail(e) {
+    e.preventDefault()
+
+    if (name === '' || email === '' || message === '') {
+      alert('Por favor, preencha todos os campos')
+      return
+    }
+
+    const templateParams = {
+      from_name: name,
+      message: message,
+      email: email,
+    }
+
+    emailjs
+      .send(
+        'service_gutsusg',
+        'template_dcgtfkc',
+        templateParams,
+        'Dvl6BvzreOxg5nH63'
+      )
+      .then(
+        (resp) => {
+          console.log('email enviado', resp.status, resp.text)
+          setName('')
+          setEmail('')
+          setMessage('')
+        },
+        (err) => {
+          console.log('FAILED: ', err)
+        }
+      )
+  }
+
+  return (
+    <div className="container">
+      <h1 className="title">Contato</h1>
+
+      <form className="form" onSubmit={sendEmail}>
+        <input
+          className="input"
+          type="text"
+          placeholder="Digite seu nome"
+          onChange={(e) => setName(e.target.value)}
+          value={name}
+        />
+
+        <input
+          className="input"
+          type="text"
+          placeholder="Digite seu email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+
+        <textarea
+          className="textarea"
+          placeholder="Digite sua mensagem..."
+          onChange={(e) => setMessage(e.target.value)}
+          value={message}
+        />
+
+        <input className="button" type="submit" value="Enviar" />
+      </form>
+    </div>
+  )
+}
+
+export default App
